9bcfcd8936dfb522c8e6580155cc44a08889e058
Astrid Sanchez Carrasco <100500836@alumnos.uc3m.es>
2024-11-13 10:15:16 -0700

    Correciones al achivo extractor_texto_txt

--- init_projects/identificador_texto/identificador_texto.py
+++
@@ -1,9 +0,0 @@
-
-with open(r'semblanza.txt', 'r') as file:
-        # read all content from a file using read()
-        content = file.read()
-        # check if string present or not
-        if 'Line 8' in content:
-            print('string exist')
-        else:
-            print('string does not exist')
